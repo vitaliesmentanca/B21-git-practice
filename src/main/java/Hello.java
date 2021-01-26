704d42f95655c6f6cfd75b72fb8c9de3c1d857cb
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-01-26 14:37:47 -0600

    movie 2 branch

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,12 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world b21");
-        //more comment
-        System.out.println("avatar");
-        System.out.println("nice");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
+        System.out.println("Hello world b22");
+        System.out.println("Hello world b22");
+
     }
 }
