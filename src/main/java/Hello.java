afa9a4621d8b892fd45f9e646a46274edfe22029
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-01-26 12:45:47 -0600

    added build

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
         System.out.println("Hello world");
         System.out.println("avatar");
         System.out.println("nice");
+        System.out.println("nice");
     }
 }
