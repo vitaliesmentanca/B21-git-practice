80c1541008937b63e1bedb865dffebbefa4e80f5
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-01-26 14:39:20 -0600

    movie again

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,8 +5,6 @@ public class Hello {
         System.out.println("avatar");
         System.out.println("nice");
         System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
+
     }
 }
