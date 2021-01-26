3f193cff0ba3b91377be02736465f3682d576744
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-01-26 14:16:21 -0600

    movie 1 updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world b21");
         System.out.println("avatar");
         System.out.println("nice");
         System.out.println("2");
