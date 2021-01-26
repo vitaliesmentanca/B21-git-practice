f329df521f231504711fac20b0b975266b19129f
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-01-26 14:40:11 -0600

    movie again r

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         System.out.println("Hello world b22");
         System.out.println("Hello world b22");
         System.out.println("Hello world b2");
+        System.out.println("Hello world b2");
 
     }
 }
